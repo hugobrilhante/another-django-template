feb53dd17cf14d90d19dda5293bd2238a093f2e7
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-07 14:28:57 -0300

    Api to order app

--- src/settings.py
+++ src/settings.py
@@ -41,6 +41,7 @@ class Base(Configuration):
         "django.contrib.sessions",
         "django.contrib.messages",
         "django.contrib.staticfiles",
+        "rest_framework",
         # App locals
         "src.core.apps.CoreConfig",
     ]
@@ -132,6 +133,10 @@ class Base(Configuration):
     # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+    REST_FRAMEWORK = {
+        "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.URLPathVersioning"
+    }
 
     LOGGING = {
         "version": 1,
