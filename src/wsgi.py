85b12b85ec694e59a0ba145e1ba9ce3764060545
Hugo Brilhante <hugobrilhante@gmail.com>
2024-03-13 10:24:04 -0300

    Using Prod instead Dev

--- src/wsgi.py
+++ src/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Prod')
 
 application = get_wsgi_application()
